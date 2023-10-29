2bb6f24bf7cc48c877357990c837363eee9255be
James Redding <jdr161@case.edu>
2023-10-28 20:06:17 -0400

    add error checking for microservice not started and updated ports for microservices

--- API Gateway/index.js
+++ API Gateway/index.js
@@ -1,9 +1,11 @@
-const { default: axios } = require('axios');
+const { default: axios } = require('axios')
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
 
 const app = express()
-const port = 3001;
+const port = 3001
+const participantsMSPort = 8081
+const eventsMSPort = 8080
 
 
 // we are only sending json requests
@@ -13,45 +15,61 @@ app.use(express.json())
 app.use(cors());
 
 app.get('/api/view-events', (req, res) => {
-    let apiURL = `http://localhost:8080/api/view-events`
+    let apiURL = `http://localhost:${eventsMSPort}/api/view-events`
     axios.get(apiURL).then(function (response){
         res.status(response.status).send(response.data)
     }).catch(function (error){
         console.log(error)
-        res.status(error.response.status).send(error.response.data)
+        if(error.code == 'ECONNREFUSED'){
+            res.status(504).send({message: "Could not connect to event microservice"})
+        } else {
+            res.status(error.response.status).send(error.response.data)
+        }  
     })
 })
 
 app.post('/api/create-event', (req, res) => {
-    let apiURL = `http://localhost:8080/api/create-event`
+    let apiURL = `http://localhost:${eventsMSPort}/api/create-event`
 
     axios.post(apiURL, req.body).then(function (response){
         res.status(response.status).send(response.data)
     }).catch(function (error){
         console.log(error)
-        res.status(error.response.status).send(error.response.data)
+        if(error.code == 'ECONNREFUSED'){
+            res.status(504).send({message: "Could not connect to event microservice"})
+        } else {
+            res.status(error.response.status).send(error.response.data)
+        }  
     })
 })
 
 app.get('/api/view-participants', (req, res) => {
-    let apiURL = `http://localhost:8080/api/view-participants`
+    let apiURL = `http://localhost:${participantsMSPort}/api/view-participants`
 
     axios.get(apiURL, req.body).then(function (response){
         res.status(response.status).send(response.data)
     }).catch(function (error){
         console.log(error)
-        res.status(error.response.status).send(error.response.data)
+        if(error.code == 'ECONNREFUSED'){
+            res.status(504).send({message: "Could not connect to participant microservice"})
+        } else {
+            res.status(error.response.status).send(error.response.data)
+        }  
     })
 })
 
 app.post('/api/create-participant', (req, res) => {
-    let apiURL = `http://localhost:8080/api/create-participant`
+    let apiURL = `http://localhost:${participantsMSPort}/api/create-participant`
 
     axios.post(apiURL, req.body).then(function (response){
         res.status(response.status).send(response.data)
     }).catch(function (error){
         console.log(error)
-        res.status(error.response.status).send(error.response.data)
+        if(error.code == 'ECONNREFUSED'){
+            res.status(504).send({message: "Could not connect to participant microservice"})
+        } else {
+            res.status(error.response.status).send(error.response.data)
+        }  
     })
 })
 
@@ -59,13 +77,17 @@ app.put('/api/register-participant', (req, res) => {
     let participantId = req.query.participantId
     let eventId = req.query.eventId
 
-    let apiURL = `http://localhost:8080/api/register-participant?participantId=${participantId}&eventId=${eventId}`
+    let apiURL = `http://localhost:${eventsMSPort}/api/register-participant?participantId=${participantId}&eventId=${eventId}`
 
     axios.put(apiURL).then(function (response){
         res.status(response.status).send(response.data)
     }).catch(function (error){
         console.log(error)
-        res.status(error.response.status).send(error.response.data)
+        if(error.code == 'ECONNREFUSED'){
+            res.status(504).send({message: "Could not connect to event microservice"})
+        } else {
+            res.status(error.response.status).send(error.response.data)
+        }  
     })
 })
 
